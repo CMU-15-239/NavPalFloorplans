43deb149497740429ee5394e830fb2edfe9e542c
Vansi Vallabhaneni <vansivallab@gmail.com>
2013-02-15 21:53:21 -0500

    map generation working

--- Flow/www/js/text/map.js
+++ Flow/www/js/text/map.js
@@ -1,47 +1,81 @@
-function addLines(space, map) {
+function removeDoors(space, map, doorChar) {
 	for (var d=0; d<space.doors.length; d++) {
-		var door = doors[d];
+		var door = space.doors[d];
 		var x1 = door.p1.x;
 		var x2 = door.p2.x;
+		var y1 = door.p1.y;
+		var y2 = door.p2.y;
 		var minX = Math.floor(Math.min(x1, x2))
 		var maxX = Math.ceil(Math.max(x1, x2))
+		var minY = Math.floor(Math.min(y1, y2))
+		var maxY = Math.ceil(Math.max(y1, y2))
 		// Put line in form y = mx + b
 		var m = -door.a / door.b
 		var b = -door.c / door.b
 		for (var x=minX; x<maxX; x++) {
 			var y = m*x + b;
 			var epsilon = Math.abs(Math.round(y) - y);
-			if (epsilon > 0.2) {
-				map[Math.floor(y)][x] = 'O';
-				map[Math.ceil(y)][x] = 'O';
+			if (epsilon >= 0) {
+				map[Math.floor(y)][x] = doorChar;
+				map[Math.ceil(y)][x] = doorChar;
 			}
 			else {
-				map[Math.round(y)][x] = 'O';
+				map[Math.round(y)][x] = doorChar;
+			}
+		}
+		var m = -door.b / door.a;
+		var b = -door.c / door.a;
+		for (var y=minY; y<maxY; y++) {
+			var x = m*y + b;
+			var epsilon = Math.abs(Math.round(y) - y);
+			if (epsilon >= 0) {
+				map[y][Math.floor(x)] = doorChar;
+				map[y][Math.ceil(x)] = doorChar;
+			}
+			else {
+				map[y][Math.round(x)] = doorChar;
 			}
 		}
 	}
 	return map;
 }
 
-function addLines(space, map) {
+function addLines(space, map, wallChar) {
+	console.log(map);
 	for (var w=0; w<space.walls.length; w++) {
-		var wall = walls[w];
+		var wall = space.walls[w];
 		var x1 = wall.p1.x
 		var x2 = wall.p2.x;
+		var y1 = wall.p1.y;
+		var y2 = wall.p2.y;
 		var minX = Math.floor(Math.min(x1, x2))
 		var maxX = Math.ceil(Math.max(x1, x2))
-		// Put line in form y = mx + b
+		var minY = Math.floor(Math.min(y1, y2))
+		var maxY = Math.ceil(Math.max(y1, y2))		// Put line in form y = mx + b
 		var m = -wall.a / wall.b
 		var b = -wall.c / wall.b
 		for (var x=minX; x<maxX; x++) {
 			var y = m*x + b;
 			var epsilon = Math.abs(Math.round(y) - y);
-			if (epsilon > 0.2) {
-				map[Math.floor(y)][x] = 'X';
-				map[Math.ceil(y)][x] = 'X';
+			if (epsilon >= 0) {
+				map[Math.floor(y)][x] = wallChar;
+				map[Math.ceil(y)][x] = wallChar;
 			}
 			else {
-				map[Math.round(y)][x] = 'X';
+				map[Math.round(y)][x] = wallChar;
+			}
+		}
+		var m = -wall.b / wall.a;
+		var b = -wall.c / wall.a;
+		for (var y=minY; y<maxY; y++) {
+			var x = m*y + b;
+			var epsilon = Math.abs(Math.round(y) - y);
+			if (epsilon >= 0) {
+				map[y][Math.floor(x)] = wallChar;
+				map[y][Math.ceil(x)] = wallChar;
+			}
+			else {
+				map[y][Math.round(x)] = wallChar;
 			}
 		}
 	}
@@ -49,21 +83,23 @@ function addLines(space, map) {
 }
 
 function map(spaces) {
-	var rows = 15;
-	var cols = 15;
+	var rows = 50;
+	var cols = 50;
 	var map = [];
+	var doorChar = '0';
+	var wallChar = 'X';
 	for (var row=0; row<rows; row++) {
 		mapRow = [];
 		for (var col=0; col<cols; col++) {
-			mapRow.push('O');
+			mapRow.push("_");
 		}
 		map.push(mapRow);
 	}
 	for(var s = 0; s < spaces.length; s++) {
-		addLines(spaces[s], map)
+		addLines(spaces[s], map, wallChar)
 	}
 	for(var s = 0; s < spaces.length; s++) {
-		removeEntrances(spaces[s], map)
+		removeDoors(spaces[s], map, doorChar)
 	}
 	return map;
 }
--- Flow/www/js/text/sector.js
+++ Flow/www/js/text/sector.js
@@ -1,1 +1,18 @@
-var RoomIdentifiers = new Object();
+function fillSector(space, sector) {
+	var vertices = space.points;
+	var hit = false;
+	while (!hit) {
+
+	}
+}
+
+
+function sector(spaces, map) {
+	var rows = map.length;
+	var cols = map[0].length;
+	var sector = map;
+	for(var s = 0; s < spaces.length; s++) {
+		sector = fillSector(spaces[s], sector)
+	}
+	return sector;
+}
