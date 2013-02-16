bc89c5677efbb230964529691bee1ddebbe18d26
Vansi Vallabhaneni <vansivallab@gmail.com>
2013-02-16 12:17:35 -0500

    findPointInShape working, but floodFillShape doesnt

--- Flow/www/js/text/sector.js
+++ Flow/www/js/text/sector.js
@@ -1,48 +1,102 @@
-function onWall(point, line) {
-	((this.p1.x >= point.x && point.x >= this.p2.x) ||
-	 (this.p1.x <= point.x && point.x <= this.p2.x)) &&
-	 ((this.p1.y >= point.y && point.y >= this.p2.y) ||
-	 (this.p1.y <= point.y && point.y <= this.p2.y));
+//sector.js
+
+
+function pointOnLines(point, lines) {
+	for(var l = 0; l < lines.length; l++) {
+		if(lines[l].pointNearLine(point, 0)) {
+			return true;
+		}
+	}
+	return false;
 }
 
-function fillSector(space, sector) {
-	var vertices = space.points;
-	var hitPoint;
-	var hit = false;
-	var v = 0;
-	while (!hit && v<vertices.length) {
-		var vertex = vertices[v];
-		var hitVertices = []
-		var startRow = Math.max(0, vertex.x - 1)
-		var startCol = Math.max(0, vertex.y - 1)
-		var endRow = Math.min(sector.length, vertex.x + 1)
-		var endCol = Math.min(sector[0].length, vertex.y + 1)
-		for (var r=startRow; r<endRow; r++) {
-			for (var c=startCol; c<endCol; c++) {
-				for (var w=0; w<space.walls.length; w++) {
-					if (c !== vertex.x && r !== vertex.y) {
-						var point = new Point(r, c);
-						if (onWall(point, space.walls[w])) {
-							hitVertices.append(point)
-						}
-					}
+function pointInShape(point, lines, width, height) {
+	var inShapeSegments = [];
+	if(!pointOnLines(point, lines)) {
+		var currP1 = null;
+		for(var rx = 0; rx < width; rx++) {
+			var currRayPt = {x: rx, y: point.y};
+			if(pointOnLines(currRayPt, lines)) {
+				if(util.exists(currP1)) {
+					inShapeSegments.push(new Line(currP1, new Point(currRayPt.x, currRayPt.y)));
+					currP1 = null;
+				}
+				else {
+					currP1 = new Point(currRayPt.x, currRayPt.y);
 				}
 			}
 		}
-		if (hitVertices.length === 3) {
-			hit = true;
-		}
 	}
 	
+	return pointOnLines(point, inShapeSegments);
+}
+
+function findPointInShape(lines, width, height) {
+	var checkedVertices = [];
+	
+	for(var l = 0; l < lines.length; l++) {
+		var endPts = [lines[l].p1, lines[l].p2];
+		for(var ep = 0; ep < endPts.length; ep++) {
+			var vertex = endPts[ep];
+			var vertexString = JSON.stringify(vertex);
+			if(checkedVertices.indexOf(vertexString) == -1) {
+				for(var dy = -1; dy <= 1; dy++) {
+					for(var dx = -1; dx <= 1; dx++) {
+						//if(dy != 0 && dx != 0) {
+							var point = {x: vertex.x+dx, y: vertex.y+dy};
+							if(0 <= point.x && point.x < width
+								&& 0 <= point.y && point.y < height
+								&& pointInShape(point, lines, width, height)) {
+								return point;
+							}
+						//}
+					}
+				}
+				checkedVertices.push(vertexString);
+			}
+		}
+	}
+	console.log(checkedVertices);
+}
+
+function floodFillShape(sector, lines, point, fillVal, emptyVal) {
+	var checkPts = [{x: point.x-1, y: point.y}, {x: point.x+1, y: point.y},
+					{x: point.x, y: point.y-1}, {x: point.x, y: point.y+1}];
+	
+	for(var cp = 0; cp < checkPts.length; cp++) {
+		var fPoint = checkPts[cp];
+		if(0 <= fPoint.x && fPoint.x <= sector[0].length
+			&& 0 <= fPoint.y && fPoint.y <= sector.length
+			&& sector[fPoint.y][fPoint.x] === emptyVal) {
+			sector[fPoint.y][fPoint.x] = fillVal;
+			if(!pointOnLines(fPoint, lines)) {
+				floodFillShape(sector, lines, fPoint, fillVal, emptyVal);
+			}
+		}
+	}
+					
+}
+
+function fillSector(sector, lines, fillVal, emptyVal) {
+	var point = findPointInShape(lines, sector[0].length, sector.length);
+	floodFillShape(sector, lines, point, fillVal, emptyVal);
 }
 
 
-function sector(spaces, map) {
-	var rows = map.length;
-	var cols = map[0].length;
-	var sector = map;
-	for(var s = 0; s < spaces.length; s++) {
-		sector = fillSector(spaces[s], sector)
+function sector(spaces, width, height) {
+	var sector = [];
+	var emptyChar = '-1';
+	for (var row=0; row < width; row++) {
+		sectorRow = [];
+		for (var col=0; col < height; col++) {
+			sectorRow.push(emptyChar);
+		}
+		sector.push(sectorRow);
 	}
+	
+	for(var s = 0; s < spaces.length; s++) {
+		fillSector(sector, spaces[s].walls, sector, ""+s, emptyChar)
+	}
+	
 	return sector;
 }
