4684556ca59a3c298ee3fab8b7ea996c43e0e69b
Vansi Vallabhaneni <vansivallab@gmail.com>
2013-02-17 21:55:45 -0500

    use space's first vertex in generateSector

--- Flow/www/js/text/sector.js
+++ Flow/www/js/text/sector.js
@@ -1,37 +1,25 @@
 //sector.js
 
-function findPointInShape(space, width, height) {
-	var checkedVertices = [];
-	
-	for(var l = 0; l < space.walls.length; l++) {
-		var endPts = [space.walls[l].p1, space.walls[l].p2];
-		for(var ep = 0; ep < endPts.length; ep++) {
-			var vertex = endPts[ep];
-			var vertexString = JSON.stringify(vertex);
-			if(checkedVertices.indexOf(vertexString) == -1) {
-				for(var dy = -1; dy <= 1; dy++) {
-					for(var dx = -1; dx <= 1; dx++) {
-						//if(dy != 0 && dx != 0) {
-							var point = {x: vertex.x+dx, y: vertex.y+dy};
-							if(0 <= point.x && point.x < width
-								&& 0 <= point.y && point.y < height
-								&& space.pointInSpace(point, width, false)) {
-								return point;
-							}
-						//}
-					}
-				}
-				checkedVertices.push(vertexString);
-			}
+function floodFillShape(sector, space, startPoint, fillVal, emptyVal) {
+	/*var fPoints = [startPoint];
+	while(fPoints.length > 0) {
+		var fPoint = fPoints.splice(0,1);
+		if(0 <= fPoint.x && fPoint.x < sector[0].length
+			&& 0 <= fPoint.y && fPoint.y < sector.length
+			&& space.pointInSpace(fPoint, sector[0].length, true)
+			&& sector[fPoint.y][fPoint.x] === emptyVal) {
+			sector[fPoint.y][fPoint.x] = fillVal;
+			checkPts = checkPts.concat([{x: fPoint.x-1, y: fPoint.y},
+					{x: fPoint.x+1, y: fPoint.y}, {x: fPoint.x, y: fPoint.y-1},
+					{x: fPoint.x, y: fPoint.y+1}]);
 		}
 	}
-	//console.log(checkedVertices);
-	return null;
-}
-
-function floodFillShape(sector, space, point, fillVal, emptyVal) {
-	var checkPts = [{x: point.x-1, y: point.y}, {x: point.x+1, y: point.y},
-					{x: point.x, y: point.y-1}, {x: point.x, y: point.y+1}];
+	console.log(sector);
+	return sector;
+	*/
+	
+	var checkPts = [{x: startPoint.x-1, y: startPoint.y}, {x: startPoint.x+1, y: startPoint.y},
+					{x: startPoint.x, y: startPoint.y-1}, {x: startPoint.x, y: startPoint.y+1}];
 	
 	//console.log("flood filling: "+JSON.stringify(point));
 	for(var cp = 0; cp < checkPts.length; cp++) {
@@ -44,14 +32,13 @@ function floodFillShape(sector, space, point, fillVal, emptyVal) {
 			floodFillShape(sector, space, fPoint, fillVal, emptyVal);
 		}
 	}
-	return sector;			
+	return sector;
 }
 
 function fillSector(sector, space, fillVal, emptyVal) {
-	var point = findPointInShape(space, sector[0].length, sector.length);
-	sector[point.y][point.x] = fillVal;
-	if(util.exists(point) && util.exists(point.x) && util.exists(point.y)) {
-		return floodFillShape(sector, space, point, fillVal, emptyVal);
+	var startPoint = space.walls[0].p1;
+	if(util.exists(startPoint) && util.exists(startPoint.x) && util.exists(startPoint.y)) {
+		return floodFillShape(sector, space, startPoint, fillVal, emptyVal);
 	}
 	else {
 		//alert("did not draw shape with lines: "+JSON.stringify(lines));
