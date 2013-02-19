3216549fad95ced41dac4f01e1722b8e32b6f745
Justin Greet <Justin@Justin-PC.(none)>
2013-02-19 15:38:48 -0500

    Keep documenting.

--- Flow/www/js/draw/Draw.js
+++ Flow/www/js/draw/Draw.js
@@ -1,26 +1,47 @@
+/* The following are globals used exclusively in this file. */
+
+//The most recently drawn point (not the one that moves with the cursor)
 var LAST_POINT;
 
+//The point that follows the user's cursor in the draw tool
 var CUR_POINT;
 
+//The line that is generated from the last point to current point
 var CUR_LINE;
 
+//The line the user is about to snap to, if it exists
 var SNAPPED_TO_LINE;
 
+//Can the user snap to the most recently drawn point?
 var CAN_SNAP_TO_LAST = false;
 
+//True iff the user is in snapping range of a point
 var ABOUT_TO_SNAP_TO_POINT = false;
 
 var DISPLAY_DOT = true;
 
+//An array of the lines that are currently selected.
 var SELECTED_LINES = [];
 
+//Is the mouse down?
 var MOUSEDOWN = false;
+
+//Is the control button currently depressed?
 var CNTRL_DOWN = false;
+
 var PREVPOINT = new Point(0,0);
+
+//The selection rectangle
 var SELECT_RECT = {shouldDraw : false, p1: undefined, p2: undefined}
 
+//Float equality!
 var epsilon = 0.0000001;
 
+/**
+ * Summary: The event handler for mouse down.
+ * Parameters: event: The object describing the event that occured.
+ * Returns: undefined
+**/
 function mouseDown(event) {
 	var newlySelectedPoints = [];
 	if (STATE === "select_tool") {
@@ -214,38 +235,55 @@ function selectToolMouseMoved(cursorX, cursorY) {
 	}
 }
 
+/**
+ * Summary: Check whether the given point is inside the point that define the 
+ *		top-left and bottom-right of a rectangle.
+ * Parameters: pointToCheck: The point to check,
+		boundingP1: The top-left point of the rectangle to check. 
+		boundingP2: The bottom-right point of the rectangle to check.
+ * Returns: true iff the given point is inside the defined rectangle.
+**/
 function pointInRect(pointToCheck, boundingP1, boundingP2) {
 	return (((pointToCheck.x >= boundingP1.x && pointToCheck.x <= boundingP2.x) || (pointToCheck.x <= boundingP1.x && pointToCheck.x >= boundingP2.x)) &&
 	((pointToCheck.y >= boundingP1.y && pointToCheck.y <= boundingP2.y) || (pointToCheck.y <= boundingP1.y && pointToCheck.y >= boundingP2.y)));
 }
-	
+
+/**
+ * Summary: The event handler when the mosue is moved in the line_tool state
+ * Parameters: cursorX: The x-coordinate of the mouse,
+		cursorY: The y-coordinate of the mouse
+ * Returns: undefined
+**/
 function lineToolAction(cursorX, cursorY) {
+	//Update the current point to be where the cursor is.
 	CUR_POINT = new Point(cursorX, cursorY);
 
 	var snapFound = false;
+	//Check if the current point is in snapping distance to a line.
 	for (var i = 0; i < ALL_WALLS.length; i++) {
 		var line = ALL_WALLS[i];
-		//console.log(line.distanceToPoint(CUR_POINT));
 		if (line.distanceToPoint(CUR_POINT) <= SNAP_RADIUS) {
+			//Now actually snap to the line.
 			if (line.snapToLine(CUR_POINT)) {
 				SNAPPED_TO_LINE = line;
 				snapFound = true;
 			}
 		}
 	}
+	//If we don't snap to a line, then leave the SNAPPED_TO_LINE undefined.
 	if (!snapFound) {
 		SNAPPED_TO_LINE = undefined;
 	}
 	
+	//Keep track of the index of the most recently-drawn point
 	var lastSnapIndex = ALL_POINTS.length - 1;
 	//If the user is allowed to snap to the most recently drawn point, accommodate that
 	//in the last index we check before snapping.
 	if (CAN_SNAP_TO_LAST) lastSnapIndex += 1;
-	//console.log(ACTIVE_SPACE.points[0]);
+	//Check if the current point is in snapping distance to a point.
 	for (var i = 0; i < lastSnapIndex; i++) {
 		var p = ALL_POINTS[i];
-		//p.setSnap(false); // TODO: this does not suffice, must set ALL snaps to false
-		// If a point is close enough, SNAP it
+		// If a point is close enough, snap to it
 		if (CUR_POINT.distance(p) <= SNAP_RADIUS) {
 			CUR_POINT.x = p.x;
 			CUR_POINT.y = p.y;
@@ -256,6 +294,7 @@ function lineToolAction(cursorX, cursorY) {
 		ABOUT_TO_SNAP_TO_POINT = false;
 	}
 	
+	//The first time through, if the line to the user's cursor isn't drawn, draw it.
 	if (LAST_POINT !== undefined) {
 		CUR_LINE = new Line(LAST_POINT, CUR_POINT);
 		CUR_LINE.draw(true);
@@ -264,14 +303,21 @@ function lineToolAction(cursorX, cursorY) {
 	CUR_POINT.draw();
 }
 
+/**
+ * Summary: The event handler for key press.
+ * Parameters: event: The object describing the event that occured.
+ * Returns: undefined
+**/
 function mouseClicked(event) {
 	redraw();
 	if (STATE === "line_tool") {
+		//If the user clicked and is not about to snap to a point, draw a new point.
 		if (ABOUT_TO_SNAP_TO_POINT === false && CUR_POINT !== undefined) {
 			//Now we know that the current point will be permanent on the drawn floor plan.
 			ALL_POINTS.push(CUR_POINT);
 		}
 		else {
+			//The user is about to snap to an existing point
 			if (CUR_LINE !== undefined) {
 				CUR_LINE.p2 = ABOUT_TO_SNAP_TO_POINT;
 				//CUR_POINT = new Point(ABOUT_TO_SNAP_TO_POINT.x, ABOUT_TO_SNAP_TO.y);
@@ -285,6 +331,7 @@ function mouseClicked(event) {
 			}
 			
 		} 
+		//This structure seriously needs to be changed.
 		if (SNAPPED_TO_LINE !== undefined && SNAPPED_TO_LINE !== true && ABOUT_TO_SNAP_TO_POINT === false) {
 			//break line into two based on current point
 			//Don't break up if the one of the new lines we'd create has length of 0.
@@ -306,59 +353,85 @@ function mouseClicked(event) {
 				if (!newLine2Duplicate) ALL_WALLS.push(twoNewLines.l2);
 			}
 		}
+		//The user just made a new line.
 		if (LAST_POINT !== undefined && CUR_LINE !== undefined) {
 			//console.log("p1: (" + CUR_LINE.p1.x + ", " + CUR_LINE.p1.y + ")    p2: (" + CUR_LINE.p2.x + ", " + CUR_LINE.p2.y + ")");
 			ALL_WALLS.push(CUR_LINE);
 			CUR_LINE.p1.degree += 1;
 			CUR_LINE.p2.degree += 1;
 		}
+		//Ensure the labeling form doesn't appear.
 		$("#done").attr("disabled", true);
-		//LAST_POINT = new Point(CUR_POINT.x, CUR_POINT.y);
+		//Update the drawing variables.
 		LAST_POINT = CUR_POINT;
 		CAN_SNAP_TO_LAST = false;
 		SNAPPED_TO_LINE = true;
 	}
 	else if (STATE === "select_tool") {
+		//If the user clicks in a room, have the classification form appear where they click.
 		if (ACTIVE_ROOM !== undefined && !BLOCK_CHANGE_ROOM && !inSnapRange(event)) {
 			$("#classification_pop").css({
 				display: "block",
-				top: event.pageY - CANVAS.y + "px",
-				left: event.pageX - CANVAS.x + "px"
+				top: event.pageY + "px",
+				left: event.pageX + "px"
 			});
+			//Populate the fields of the form with the room's information, if it exists.
 			$("#label").val(ACTIVE_ROOM.label);
 			var roomType = ACTIVE_ROOM.type;
 			if ($("#" + roomType).length) $("#" + roomType).prop('checked', true);
+			//Ensure the user can't change the active room.
 			BLOCK_CHANGE_ROOM = true;
 		}
 	}
-	//console.log("NUM SELECTED LINES: " + SELECTED_LINES.length);
 	drawWalls();
 }
 
+/**
+ * Summary: Check if the event location is within snapping distance of 
+ *		any points or walls.
+ * Parameters: event: The event to check the location of.
+ * Returns: true iff the event took place within snapping distance of a point or wall..
+**/
 function inSnapRange(event) {
+	//Get the point of the event relative to the canvas.
 	var curP = new Point(event.pageX - CANVAS.x, event.pageY - CANVAS.y);
+	//First check if the point is within snapping distance of any wall.
 	for (var i = 0; i < ALL_WALLS.length; i++) {
 		var curWall = ALL_WALLS[i];
 		if(curWall.pointNearLine(curP, SNAP_RADIUS)) {
 			return true;
 		}
 	}
+	//Check if the point is within snapping distance of any point.
 	for (var i = 0; i < ALL_POINTS.length; i++) {
 		var curPoint = ALL_POINTS[i];
 		if (curPoint.distance(curP) <= SNAP_RADIUS) return true;
 	}
+	//The point isn't close to any point or wall.
 	return false;
 }
 
+/**
+ * Summary: Check whether all the closed spaces have been given a type
+ *		by the user.
+ * Parameters: none
+ * Returns: true iff all rooms have a non-empty type classification.
+**/
 function allSpacesClassified() {
 	for (var i = 0; i < ALL_CLOSED_ROOMS.length; i++) {
 		var curRoom = ALL_CLOSED_ROOMS[i];
+		//If the type hasn't yet been assigned, return false.
 		if (curRoom.type === "") return false;
 	}
+	//All the spaces have a type that isn't empty.
 	return true;
 }
 
-//Draw the walls
+/**
+ * Summary: Draw the walls and their defining points.
+ * Parameters: none
+ * Returns: undefined
+**/
 function drawWalls() {
 	for (var i = 0; i < ALL_WALLS.length; i++) {
 		ALL_WALLS[i].draw();
@@ -368,23 +441,43 @@ function drawWalls() {
 	}
 }
 
+/**
+ * Summary: Draw the background image that is the original floorplan.
+ * Parameters: none
+ * Returns: undefined
+**/
 function drawFloorPlan() {
 	if (window.FLOOR_PLAN != null) {
 		CANVAS.drawImage(FLOOR_PLAN,0,0, CANVAS.width, CANVAS.height);
 	}
 }
 
+/**
+ * Summary: Draw the polygons that define closed spaces, if appropriate.
+ * Parameters: none
+ * Returns: undefined
+**/
 function drawSpaces() {
 	for (var i = 0; i < ALL_CLOSED_ROOMS.length; i++) {
 		ALL_CLOSED_ROOMS[i].draw();
 	}
 }
 
+/**
+ * Summary: Redraw the volatile elements on the canvas.
+ * Parameters: none
+ * Returns: undefined
+**/
 function redraw() {
+	//First, delete everything from the canvas.
     CANVAS.clearRect(0, 0, CANVAS.width, CANVAS.height);
+	//Draw the background floorplan image
     drawFloorPlan();
+	//Draw all the walls and the points that define them.
 	drawWalls();
+	//Draw all the spaces (the polygons that define them)
 	drawSpaces();
+	//Draw the selection rectangle if appropriate.
 	if (SELECT_RECT.shouldDraw && !SELECT_RECT.p1.equals(SELECT_RECT.p2)) {
 		var p1 = SELECT_RECT.p1;
 		var p2 = SELECT_RECT.p2;
@@ -397,23 +490,32 @@ function redraw() {
 	}
 }
 
+/**
+ * Summary: The event handler for key press.
+ * Parameters: event: The object describing the event that occured.
+ * Returns: undefined
+**/
 function keyPressed(event) {
+	//Get the code of the key that was pressed.
 	var keyCode = event.keyCode;
-	//Break the current line (i.e. stop the drawing tool
+	//space or enter
 	if (keyCode === 13 || keyCode === 32) {
 		redraw();
+		//Break the current line (i.e. stop the drawing tool)
 		if (STATE === "line_tool") {
 			//STATE = "select_tool";
 			CUR_POINT = undefined;
 			resetLineGlobals();
 			CAN_SNAP_TO_LAST = true;
 		}
+		//Unselect everything.
 		if (STATE === "select_tool") {
 			unselectAll();
 		}
 	}
 	//'d'
 	if (keyCode === 100) {
+		//Toggle whether the selected lines are doors.
 		if (STATE === "select_tool") {
 			for (var i = 0; i < ALL_WALLS.length; i++) {
 				var curWall = ALL_WALLS[i];
@@ -421,6 +523,7 @@ function keyPressed(event) {
 			}
 			redraw();
 		}
+		//Toggle whether the most recently-drawn line is a door.
 		else if (STATE === "line_tool") {
 			if (ALL_WALLS.length >= 1) {
 				var mostRecentWall = ALL_WALLS[ALL_WALLS.length - 1];
@@ -433,7 +536,13 @@ function keyPressed(event) {
 	}
 }
 
+/**
+ * Summary: The event handler for key down.
+ * Parameters: event: The object describing the event that occured.
+ * Returns: undefined
+**/
 function keyDown(event) {
+	//Get the code of the key that was pressed.
 	var keyCode = event.keyCode;
 	//Ctrl key
 	if (keyCode === 17) {
@@ -441,7 +550,13 @@ function keyDown(event) {
 	}
 }
 
+/**
+ * Summary: The event handler for key up.
+ * Parameters: event: The object describing the event that occured.
+ * Returns: undefined
+**/
 function keyUp(event) {
+	//Get the code of the key that was released.
 	var keyCode = event.keyCode;
 	//Ctrl key
 	if (keyCode === 17) {
@@ -454,7 +569,8 @@ function keyUp(event) {
 			var pointsToDelete = [];
 			for (var i = 0; i < ALL_POINTS.length; i++) {
 				var p = ALL_POINTS[i];
-				if (p.isSelected) {
+				//If the point is currently selected, then it should be deleted.
+				if (p.isSelected === true) {
 					pointsToDelete.push(p);
 					//Remove the point from the points array.
 					ALL_POINTS.splice(i, 1);
@@ -462,17 +578,21 @@ function keyUp(event) {
 					i -= 1;
 				}
 			}
-			//Now delete all the walls that contain a point to delete.
+			//Now delete all the walls that are either selected or contain a 
+			//point to delete, as a wall shouldn't exist without two defined endpoints.
 			var numDeletedWalls = 0;
 			for (var j = 0; j < ALL_WALLS.length; j++) {
 				var l = ALL_WALLS[j];
 				if (l.isSelected) {
+					//Set the position in the array to false to indicate that it should be removed.
 					ALL_WALLS[j] = false;
 					numDeletedWalls += 1;
+					//Decrement the degree of each of its endpoints.
 					l.p1.degree -= 1;
 					l.p2.degree -= 1;
 					continue;
 				}
+				//Now go through and delete the walls with a deleted endpoint.
 				for (var m = 0; m < pointsToDelete.length; m++) {
 					var p = pointsToDelete[m];
 					if (l.p1.equals(p) || l.p2.equals(p)) {
@@ -491,12 +611,13 @@ function keyUp(event) {
 				if (l === false) {
 					ALL_WALLS.splice(index, 1);
 					numDeletedWalls -= 1;
+					//Note that the size of ALL_WALLS just changed.
 					index -= 1;
 				}
 				index += 1;
 			}
 			
-			//Delete the point because it's not connected to any line
+			//Delete points that have degree 0 (i.e. aren't connected to anything)
 			for (var i = 0; i < ALL_POINTS.length; i++) {
 				var p = ALL_POINTS[i];
 				if (p.degree <= 0) {
@@ -510,6 +631,11 @@ function keyUp(event) {
 	}
 }
 		
+/**
+ * Summary: Deselect all the points and walls.
+ * Parameters: none
+ * Returns: undefined
+**/
 function unselectAll() {
 	for (var i = 0; i < ALL_WALLS.length; i++) {
 		ALL_WALLS[i].isSelected = false;
@@ -520,43 +646,30 @@ function unselectAll() {
 	redraw();
 }
 
+/**
+ * Summary: Reset the globals used by the drawing tool.
+ * Parameters: none
+ * Returns: undefined
+**/
 function resetLineGlobals() {
 	LAST_POINT = undefined;
-	//CUR_POINT = undefined;
 	CUR_LINE = undefined;
 }
 
-// Make the 'Add Room' button active.
-function enableAddRoom() {
-	$("#add_room").removeAttr("disabled");
-}
-
-// Make the 'Add Room' button inactive.
-function disableAddRoom() {
-	$("#add_room").attr("disabled", "true");
-}
-
-// This will be called when the 'Add Room' button is active and clicked.
-function addRoomClicked() {
-	var newSpace = new Space(SELECTED_LINES);
-	ALL_CLOSED_ROOMS.push(newSpace);
-	/*for (var j = 0; j < newSpace.walls.length; j++) {
-		console.log("Wall # " + (j+1) + " in room: " + newSpace.walls[j]);
-	}*/
-	for (var i = 1; i <= ALL_CLOSED_ROOMS.length; i++) {
-		if (i === ALL_CLOSED_ROOMS.length) ALL_CLOSED_ROOMS[i-1].drawPoly = true;
-		else ALL_CLOSED_ROOMS[i-1].drawPoly = false;
-	}
-	redraw();
-}
-
+/**
+ * Summary: The method called right before sending data to the post-processor that
+ *		populates the door array in each room.
+ * Parameters: none
+ * Returns: undefined
+**/
 function addDoorsToRooms() {
 	for (var i = 0; i < ALL_CLOSED_ROOMS.length; i++) {
 		var curSpace = ALL_CLOSED_ROOMS[i];
+		//Iterate through all the walls for the room and push those that
+		//are doors
 		for (var j = 0; j < curSpace.walls.length; j++) {
 			var curWall = curSpace.walls[j];
 			if (curWall.isDoor) curSpace.doors.push(curWall);
 		}
-		console.log(curSpace.doors.length);
 	}
 }
