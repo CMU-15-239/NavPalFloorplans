99f9951133c4ec38247503dd1c126f192152af95
Daniel <daniel@daneil-Dell-System-XPS-L502X.(none)>
2013-03-19 20:38:04 -0400

    commented my section of server code

--- Flow/server/server.js
+++ Flow/server/server.js
@@ -1,3 +1,8 @@
+/*  Node.js server for User Authoring tool
+    Written by: Daniel Muller and Vansi Vallabhaneni (2013)
+*/
+
+// initialize middleware and global variables
 var path = require('path');
 var express = require('express');
 var http = require('http');
@@ -6,11 +11,14 @@ var exec = require('child_process').exec;
 var fs = require('fs');
 var child;
 var Sector = require('./sector.js');
-
-// set up express server
 var app = express();
 var port = process.env.PORT || 3000;
 
+/**
+ * Summary: Initiazes the express server
+ * Parameters: undefined
+ * Returns: undefined
+**/
 function init(){
     configureExpress(app);
     http.createServer(app).listen(port, function() {
@@ -20,7 +28,12 @@ function init(){
 
 init();
 
-// setup express for serving files and Access-Control workarounds
+
+/**
+ * Summary: Setup express with middleware and Access-Control workarounds
+ * Parameters: undefined
+ * Returns: undefined
+**/
 function configureExpress(app) {
     app.configure(function() {
 
@@ -31,10 +44,10 @@ function configureExpress(app) {
     	app.use(express.session({secret:"keyboard cat"}));
     	app.use(express.static(path.join(__dirname, '../www')));
 
-        app.use(function(req, res, next) {
-            res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-            res.header('Access-Control-Allow-Methods', 'PUT,GET,POST,DELETE,OPTIONS');
-            res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
+        app.use(function(request, response, next) {
+            response.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+            response.header('Access-Control-Allow-Methods', 'PUT,GET,POST,DELETE,OPTIONS');
+            response.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With');
 
             next();
         });
@@ -46,23 +59,26 @@ function configureExpress(app) {
 
 // =========== ROUTES ==========
 
-// upload an image for preprocessing.
-// REQUIRES: base64 represenation of floorplan image
-// ENSURES: list of lines is returned in JSON as well as link to greyscale image
-app.post('/upload', function (req, res) {
+/**
+ * Summary: Upload an image for preprocessing.
+ * Parameters:  req.body.image: base64 encoding of
+                request.user: passport userId   
+ * Returns: list of lines is returned in JSON as well as link to greyscale image
+**/
+app.post('/upload', function (request, response) {
 	var base64Data = req.body.image;
     var imagePath = '../www/floorplans/floorPlan.jpg';
     var index = base64Data.indexOf('base64,') + 'base64,'.length;
     base64Data = base64Data.substring(index, base64Data.length);
     fs.writeFile(imagePath, new Buffer(base64Data, "base64"));
-    preprocessor(imagePath, res);
+    preprocessor(imagePath, response);
 });
 
 // creates three text files required by navPal android app
-app.post('/text', function (req, res) {
-    var map = req.body.map;
-    var room = req.body.room;
-    var id = req.body.id;
+app.post('/text', function (request, response) {
+    var map = request.body.map;
+    var room = request.body.room;
+    var id = request.body.id;
     if (map !== undefined && map !== null) {
         fs.writeFile('../www/text/' + id + '_map.txt', map);
     }
@@ -72,15 +88,15 @@ app.post('/text', function (req, res) {
     if (sector !== undefined && sector !== null) {
         fs.writeFile('./www/text/' + id + '_sector.txt', sector);
     }
-    return res.send('sucess!'); 
+    return response.send('sucess!'); 
 });
 
 // creates json of graph representation of floorplan
-app.post('/graph', function (req, res) {
-    var graph = req.body.graph;
-    var width = req.body.width;
-    var height = req.body.height;
-    var id = req.body.id;
+app.post('/graph', function (request, response) {
+    var graph = request.body.graph;
+    var width = request.body.width;
+    var height = request.body.height;
+    var id = request.body.id;
     if (graph !== undefined && graph !== null) {
 		console.log("************************");
 		console.log(graph.spaceNodes.length);
@@ -92,16 +108,27 @@ app.post('/graph', function (req, res) {
 			fs.writeFile('../www/text/' + id + '_sector.txt', sector);
 		}
     }
-    return res.send('sucess!');
+    return response.send('sucess!');
 });
 
 // =========== PREPROCESSOR ==========
 
-// calls python script to extract lines from image and convert to gray scale
-function preprocessor(imagePath, res) {
+/*  TODO:
+        temp id for 
+
+*/
+
+
+/**
+ * Summary: calls python script to extract lines from image and convert to gray scale
+ * Parameters:  imagePath: path to image to be processsed
+                response: responseponse to be sent to user 
+ * Returns: response with json of identified lines and src of thresholded image
+**/
+function preprocessor(imagePath, response) {
     var linesJSON;
-    child = exec('python preprocessing.py ' + imagePath,
-    function (error, stdout, stderr) {
+    child = exec('python preprocessing.py ' + imagePath, 
+        function (error, stdout, stderr) {
         // First I want to read the file
         fs.readFile('json.txt', function read(err, data) {
             if (err) {
@@ -111,7 +138,7 @@ function preprocessor(imagePath, res) {
             var lines = JSON.parse(linesJSON);
             // start at character 6 to remove ../www
             lines['image'] = imagePath.substring(6, imagePath.length);
-            return res.json(lines);
+            return response.json(lines);
         });
     });
 }
