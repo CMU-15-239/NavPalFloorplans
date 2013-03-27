d1e8033029d3074116fb35f9d51397d59a371ea5
Vansi Vallabhaneni <vansivallab@gmail.com>
2013-03-26 20:24:55 -0400

    added floor interconnections to floor and building objects

--- Flow/www/js/draw/building.js
+++ Flow/www/js/draw/building.js
@@ -2,5 +2,7 @@
 
 function Building(name) {
   this.name = name;
+  this.id;
   this.floors = [];
+  this.floorInterconnections = [];
 }
--- Flow/www/js/draw/floor.js
+++ Flow/www/js/draw/floor.js
@@ -4,9 +4,13 @@ function Floor(name, imageId) {
   this.name = name;
   this.imageId = imageId;
   this.imageScale = 1.0;
+  
+  //Space objects
   this.spaces = [];
-  this.landmarks = [];
   this.obsticals = [];
+  
+  this.landmarks = [];
+  this.floorInterconnections = [];
 }
 
 Floor.prototype.calcScale = function(space, userSqFt) {
