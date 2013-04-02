a36e47ae96daf02e14897413db073f219dfbbb5a
Daniel Muller <dmuller@andrew.cmu.edu>
2013-04-02 11:48:36 -0400

    commented javascript

--- Flow/www/js/authentication.js
+++ Flow/www/js/authentication.js
@@ -1,9 +1,23 @@
+/**
+ * Handles login and registration requests to server
+ * Written by: Daniel Muller
+*/
+
+/**
+ * Summary: Sends post /login request to server
+ * Parameters: valid email and password combo
+ * Returns: redirect if successful orelse error message
+**/
 $("#loginButton").click(function() {
+	// make all error messages hidden
 	$(".help-block").addClass('hidden');
+	// grab input email and password
 	var logEmail = $("#loginEmail").val();
 	var logPass = $("#loginPass").val();
+	// grab elements to change in case of error, reset errors
 	var loginForm = $("#loginForm").attr('class', 'control-group');
 	var loginInfo = $("#loginInfo");
+	// send post request to server
 	$.ajax({
 		type: "POST",
 		url: '/login',
@@ -21,24 +35,36 @@ $("#loginButton").click(function() {
 	});
 })
 
+
+/**
+ * Summary: Sends post /register request to server
+ * Parameters: valid email and password combo
+ * Returns: redirect if successful orelse error message
+**/
 $("#regButton").click(function() {
+	// make all error messages hidden
 	$(".help-block").addClass('hidden');
+	// grab email and password input
 	var regEmail = $("#regEmail").val();
 	var conEmail = $("#confirmEmail").val();
 	var regPass = $("#regPass").val();
 	var confirmPass = $("#confirmPass").val();
+	// grab elements to change in case of error, reset errors
 	var matchEmails = $('#matchEmails').attr('class', 'control-group');
 	var matchPasswords = $('#matchPasswords').attr('class', 'control-group');
 	var regEmailInfo = $('#regEmailInfo');
 	var regPasswordInfo = $('#regPasswordInfo');
+	// check if emails match
 	if (regEmail !== conEmail) {
 		matchEmails.addClass('error');
 		regEmailInfo.removeClass('hidden').text('Emails do not match.');
 	}
+	// check if passwords match
 	else if (regPass !== confirmPass) {
 		matchPasswords.addClass('error');
 		regPasswordInfo.removeClass('hidden').text('Passwords do not match');
 	}
+	// send post request to server
 	else {
 		$.ajax({
 			type: "POST",
@@ -48,6 +74,7 @@ $("#regButton").click(function() {
 				password: regPass
 			},
 		}).done(function(response) {
+			// deal with server response
 			switch (response.errorCode) {
 				case 0:
 					alert ("success");
